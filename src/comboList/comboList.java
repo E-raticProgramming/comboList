e10f3bf6a324aad33de6ab1b2ac708a846860755
Connorapple <connorapple@gmail.com>
2017-05-20 14:35:42 -0400

    constructors/needed methods added

--- src/comboList/comboList.java
+++ src/comboList/comboList.java
@@ -4,10 +4,38 @@
  * Project: ComboList
  */
 package comboList;
+//possible imports:
+//import java.util.AbstractList;
+//import java.util.ArrayList;
+//import java.util.Collections;
 public class comboList {
-    public static void main(String[] args) {
-        // TODO Everything
-
+    
+    public int size;
+    public int[] data;
+    public comboList() {
+        
+    }
+    public comboList(int size) {
+        
+    }
+    public Object get(int index) {
+        return null;
+    }
+    public int size() {
+        return this.size;
+    }
+    public void add (int element) {
+        
+    }
+    public void add (int index, int element) {
+        
+    }
+    public void remove (int index) {
+        
+    }
+    //test client
+    public static void main (final String[] args) {
+        
     }
 
 }
